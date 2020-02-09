5df7bce9afbf670e1abb0dc37cf1d6faa26f3f86
Austin Lordi <all321@lehigh.edu>
2020-02-09 16:26:26 -0500

    fixed connection error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
  */
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:admin@cluster0-npoku.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
