d34525b5ae53a080f98a7db01c273313aee05c5a
Master Baitor <trh221@lehigh.edu>
2020-02-14 13:48:15 -0500

    remove conflict debug strings

--- client/src/App.js
+++ client/src/App.js
@@ -78,14 +78,11 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <button onClick={this.createCounter(1)}> New counter</button>
-=======
           <button onClick={this.createCounter}> New counter</button>
->>>>>>> ae2055915cc886c3e6cd63db1149ad2d8417afc6
           <a
             className="App-link"
             href="https://github.com/alordi/counter"
