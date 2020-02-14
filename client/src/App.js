3227f80770b0153317e74644d345d926eb9740a8
Brad DeMassa <bad321@lehigh.edu>
2020-02-14 12:29:10 -0500

    Tweaked the homepage to link to the Github page

--- client/src/App.js
+++ client/src/App.js
@@ -78,17 +78,14 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <button onClick={this.createCounter}> New counter</button>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/alordi/counter"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Link to Countr's GitHub Page
           </a>
         </header>
       </div>
